449b7406a5046e9ca9dc5e3092b0593b08e59af4
Maxim <toper.one111@gmail.com>
2023-08-17 17:20:28 +0300

    Render clients table

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,3 +1,5 @@
+import ClientsRow from './ClientsRow';
+
 import { useQuery, gql } from '@apollo/client';
 
 const GET_CLIENTS = gql`
@@ -18,7 +20,27 @@ const Clients = () => {
   if (loading) return <div>Loading...</div>;
 
   if (error) return <div>Something went wrong!</div>;
-  return <>{!loading && !error && <h1>Clients</h1>}</>;
+  return (
+    <>
+      {!loading && !error && (
+        <table className="table table-hover mt-3">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.clients.map((client) => (
+              <ClientsRow key={client.id} client={client} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 };
 
 export default Clients;
---
+++ client/src/components/ClientsRow.jsx
@@ -0,0 +1,18 @@
+import { FaTrash } from 'react-icons/fa6';
+
+const ClientsRow = ({ client }) => {
+  return (
+    <tr key={client.id}>
+      <td>{client.name}</td>
+      <td>{client.email}</td>
+      <td>{client.phone}</td>
+      <td>
+        <button className="btn btn-danger btn-sm">
+          <FaTrash />
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default ClientsRow;
