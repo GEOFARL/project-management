00ed75a9a86ef6557fc924ca58305e7c1181c9c5
Maxim <toper.one111@gmail.com>
2023-08-17 12:18:24 +0300

    Bring in GraphQL

---
+++ server/graphql/schema.js
@@ -0,0 +1,8 @@
+const { buildSchema, GraphQLObjectType } = require('graphql');
+const { projects, clients } = require('../sampleData');
+
+const schema = buildSchema(`
+
+`);
+
+module.exports = schema;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 require('dotenv').config();
 
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./graphql/schema');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: '',
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
